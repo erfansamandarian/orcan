c8bfaf44da4d6ab563336438618fb2330512b247
erfansamandarian <esamandarian922@icloud.com>
2024-06-11 12:09:25 +1000

    version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author_email="mail@erfansamandarian.com",
     url="https://erfansamandarian.com/soundbook",
     license="MIT",
-    version="1.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=["requests", "tabulate", "termcolor"],
     py_modules=["orcan"],
